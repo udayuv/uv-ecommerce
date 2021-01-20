21d1b4a6bf9ee1784951073cfee21a278e194b53
udayuv <uday.lko67@gmail.com>
2021-01-20 16:04:27 +0530

    added header page and links to it

--- src/App.js
+++ src/App.js
@@ -2,21 +2,18 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
 import './App.css';
+import Header from './components/header/header.component';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage =()=>(
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Switch>
         <Route exact path='/' component ={HomePage}/>
-        <Route exact path='/hats' component ={HatsPage}/>
+        <Route exact path='/shop' component ={ShopPage}/>
       </Switch>
     </div>
   );
